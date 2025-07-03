c9add48db231d7a58549211709656bbd4ca696af
unknown <nish_mandal@hotmail.com>
2025-07-03 13:54:55 +0100

    Added failing test

--- GitHubActions/src/test/java/com/sparta/clf/SavingsAccountTest.java
+++ GitHubActions/src/test/java/com/sparta/clf/SavingsAccountTest.java
@@ -3,7 +3,7 @@ package com.sparta.clf;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 public class SavingsAccountTest {
@@ -12,6 +12,10 @@ public class SavingsAccountTest {
         SavingsAccount myAccount = new SavingsAccount(1000, 5);
         myAccount.addInterest();
         assertEquals(1050, myAccount.getBalance(), 0.001);
+    }
+    @Test
+    public void filaingTest(){
+        assertFalse(true);
     }
 }
 
